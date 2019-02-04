639b2caec826d3decc76e9b482d9020fd67ed1b8
mehrnooshhasani <47318718+mehrnooshhasani@users.noreply.github.com>
2019-02-04 23:34:44 +0330

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,15 +3,15 @@ const secretOrKey = 'secret';
 const googleClientID =
   '578382244029-6sd0fdtkagn1bo3d3mov1u5mlu2atpme.apps.googleusercontent.com';
 const googleClientSecret = 'rhrFi7a3KqSbNvfM5QQy-P9L';
-const googleCallbackURL = 'http://localhost:3000/auth/google';
+const googleCallbackURL = 'https://chatogether.herokuapp.com/auth/google';
 
 const githubClientID = '0c38e585e37d3597f40e';
 const githubClientSecret = '57ab6c31adcdb4cd98c8c2e2c980a521c7c2b8a4';
-const githubCallbackURL = 'http://localhost:3000/auth/github';
+const githubCallbackURL = 'https://chatogether.herokuapp.com/auth/github';
 
 const linkedinClientID = '779qb7qicdoo2r';
 const linkedinClientSecret = '8ROZBfsd4AP1eXQF';
-const linkedinCallbackURL = 'http://localhost:3000/auth/linkedin';
+const linkedinCallbackURL = 'https://chatogether.herokuapp.com/auth/linkedin';
 
 module.exports = {
   secretOrKey: secretOrKey,
